82214e460e563b83bfafb096470c9b0aef5e066b
nabyungho <valoon2@naver.com>
2025-05-28 18:27:39 +0900

    [feat] query-optimization module init

---
+++ ch2-db-query-optimization/src/main/java/com/app/ch2dbqueryoptimization/Ch2DbQueryOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.app.ch2dbqueryoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2DbQueryOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch2DbQueryOptimizationApplication.class, args);
+    }
+
+}
---
+++ ch2-db-query-optimization/src/test/java/com/app/ch2dbqueryoptimization/Ch2DbQueryOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ch2dbqueryoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2DbQueryOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
